eb664c5a1cd83f4517e0dc8e8ffca8c11e4866ee
mahad-ahmed <herstalf@gmail.com>
2020-08-29 21:03:46 +0500

    Incremented version name and number

---
+++ app/src/main/java/com/atompunkapps/ominousbeepingapp/MainActivity.java
@@ -0,0 +1,211 @@
+package com.atompunkapps.ominousbeepingapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.AudienceNetworkAds;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final int REQUEST_CODE_SETTINGS = 9;
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.2F;
+    private static final int SHAKE_MIN_DELAY = 300;
+    private static final int SHAKE_TIMEOUT = 2000;
+
+    private static final int BEEP_DELAY_MIN = 25;
+    private static final int BEEP_DELAY_START = 1000;
+    private static final float DELAY_DELTA_START = 150;
+
+    private Sensor sensor;
+    private SensorManager sensorManager;
+
+    private MediaPlayer mp;
+    private long stopTime;
+
+    private ImageView imageView;
+    private int[] beeps = new int[] { R.drawable.circle_1, R.drawable.circle_2, R.drawable.circle_3, R.drawable.circle_4 };
+    private int beepIndex = 0;
+
+    private long shakeTime;
+    private int count;
+    private int shakeCount;
+    private int beepDuration;
+    private float delayDelta = DELAY_DELTA_START;
+    private boolean forceStop = false;
+
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.beep_image);
+
+        findViewById(R.id.options_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(MainActivity.this, SettingsActivity.class)
+                                .setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP),
+                        REQUEST_CODE_SETTINGS
+                );
+            }
+        });
+
+        AudienceNetworkAds.initialize(this);
+
+        adView = new AdView(this, SafeStore.PLACEMENT_ID, AdSize.BANNER_HEIGHT_50);
+
+        ((FrameLayout) findViewById(R.id.banner_container)).addView(adView);
+
+        adView.loadAd();
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        shakeCount = preferences.getInt("shake_count", 4);
+        beepDuration = preferences.getInt("beep_duration", 10) * 1000;
+
+        mp = MediaPlayer.create(this, R.raw.beep6);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            long delay = BEEP_DELAY_START;
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if(System.currentTimeMillis() > stopTime || forceStop) {
+                    imageView.setVisibility(View.GONE);
+
+                    delay = BEEP_DELAY_START;
+                    delayDelta = DELAY_DELTA_START;
+                    forceStop = false;
+                    return;
+                }
+                try {
+                    Thread.sleep(delay);
+                    delayDelta = delayDelta * 0.91f;
+                    delay -= Math.round(delayDelta);
+                    if(delay < BEEP_DELAY_MIN) {
+                        delay = BEEP_DELAY_MIN;
+                    }
+
+                    beepIndex = (beepIndex + 1) % 4;
+                    imageView.setImageResource(beeps[beepIndex]);
+
+                    mp.start();
+                }
+                catch (InterruptedException ignored) { }
+            }
+        });
+
+        Toast.makeText(this, "Shake the phone " + shakeCount + " times.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float gX = event.values[0] / SensorManager.GRAVITY_EARTH;
+        float gY = event.values[1] / SensorManager.GRAVITY_EARTH;
+        float gZ = event.values[2] / SensorManager.GRAVITY_EARTH;
+
+        //  g-force = 1 when no movement
+        double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+        if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+            final long now = System.currentTimeMillis();
+
+            if (shakeTime + SHAKE_MIN_DELAY > now) {
+                return;
+            }
+
+            //  Reset the count after timeout
+            if (shakeTime + SHAKE_TIMEOUT < now) {
+                count = 0;
+            }
+
+            shakeTime = now;
+            count++;
+
+            if(count >= shakeCount) {
+                if(count == shakeCount) {
+                    beepIndex = 0;
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView.setImageResource(beeps[beepIndex]);
+//                    mp.start(); // Move here?
+                }
+
+                stopTime = System.currentTimeMillis() + beepDuration;
+                mp.start();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE_SETTINGS && resultCode == SettingsActivity.RESULT_PREFS_CHANGED && data != null) {
+            if(data.hasExtra("shake_count_changed")) {
+                shakeCount = PreferenceManager.getDefaultSharedPreferences(this).getInt("shake_count", 4);
+            }
+            if(data.hasExtra("beep_duration_changed")) {
+                beepDuration = PreferenceManager.getDefaultSharedPreferences(this).getInt("beep_duration", 10) * 1000;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        if(!PreferenceManager.getDefaultSharedPreferences(this).getBoolean("bg_beeping", false)) {
+            if(sensorManager != null) {
+                sensorManager.unregisterListener(this);
+            }
+
+            forceStop = true;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if(!PreferenceManager.getDefaultSharedPreferences(this).getBoolean("bg_beeping", false)) {
+            count = 0;
+            forceStop = false;
+        }
+
+        if(sensorManager != null && sensor != null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/atompunkapps/ominousbeepingapp/SettingsActivity.java
@@ -0,0 +1,92 @@
+package com.atompunkapps.ominousbeepingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.android.play.core.tasks.Task;
+
+public class SettingsActivity extends AppCompatActivity {
+    static int RESULT_PREFS_CHANGED = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        private Intent resultIntent = new Intent();
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+
+            if(getActivity() == null) {
+                return;
+            }
+
+            getActivity().setResult(
+                    RESULT_PREFS_CHANGED,
+                    resultIntent
+            );
+
+            findPreference("shake_count").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    resultIntent.putExtra("shake_count_changed", true);
+                    return true;
+                }
+            });
+
+            findPreference("beep_duration").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    resultIntent.putExtra("beep_duration_changed", true);
+                    return true;
+                }
+            });
+
+            findPreference("rating").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    final ReviewManager manager = ReviewManagerFactory.create(preference.getContext());
+                    manager.requestReviewFlow().addOnCompleteListener(new OnCompleteListener<ReviewInfo>() {
+                        @Override
+                        public void onComplete(@NonNull Task<ReviewInfo> task) {
+                            if(task.isSuccessful()) {
+                                manager.launchReviewFlow(getActivity(), task.getResult());
+                            }
+                        }
+                    });
+                    return true;
+                }
+            });
+
+//            findPreference("bg_beeping").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+//                @Override
+//                public boolean onPreferenceChange(Preference preference, Object newValue) {
+//                    resultIntent.putExtra("bg_beeping_changed", true);
+//                    return true;
+//                }
+//            });
+        }
+    }
+}
