0ea5e70a87981971e2a481c9a6586234073c1079
mahad-ahmed <mahad628@gmail.com>
2024-02-02 08:16:44 +0500

    Commented out

--- app/src/main/java/com/atompunkapps/ominousbeepingapp/Monetization.java
+++ app/src/main/java/com/atompunkapps/ominousbeepingapp/Monetization.java
@@ -1,51 +1,51 @@
-package com.atompunkapps.ominousbeepingapp;
-
-import android.app.Activity;
-import android.widget.FrameLayout;
-
-import com.facebook.ads.AdSize;
-import com.facebook.ads.AdView;
-import com.facebook.ads.AudienceNetworkAds;
-
-public class Monetization {
-    private final AdView adView;
-
-    Monetization(Activity context, FrameLayout adContainer) {
-//        this.adView = adView;
-        AudienceNetworkAds.initialize(context);
-        adView = new AdView(context, SafeStore.PLACEMENT_ID, AdSize.BANNER_HEIGHT_50);
-        adContainer.addView(adView);
-        adView.loadAd();
-    }
-
-//    void initAds() {
-//        MobileAds.initialize(context, initializationStatus -> {
-////            Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
-////            for (String adapterClass : statusMap.keySet()) {
-////                AdapterStatus status = statusMap.get(adapterClass);
-////                Log.d("MyApp", String.format(
-////                        "Adapter name: %s, Description: %s, Latency: %d",
-////                        adapterClass, status.getDescription(), status.getLatency()));
-////            }
+//package com.atompunkapps.ominousbeepingapp;
 //
-//            // For testing purposes only
-////            new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("BD62403485F5FEBFDCB000D103922A0C"));
+//import android.app.Activity;
+//import android.widget.FrameLayout;
 //
-//            // Start loading ads here...
-//            AdRequest adRequest = new AdRequest.Builder().build();
-//            adView.loadAd(adRequest);
-//        });
-//    }
-
-//    void pauseAds() {
-//        adView.pause();
+//import com.facebook.ads.AdSize;
+//import com.facebook.ads.AdView;
+//import com.facebook.ads.AudienceNetworkAds;
+//
+//public class Monetization {
+//    private final AdView adView;
+//
+//    Monetization(Activity context, FrameLayout adContainer) {
+////        this.adView = adView;
+//        AudienceNetworkAds.initialize(context);
+//        adView = new AdView(context, SafeStore.PLACEMENT_ID, AdSize.BANNER_HEIGHT_50);
+//        adContainer.addView(adView);
+//        adView.loadAd();
 //    }
 //
-//    void resumeAds() {
-//        adView.resume();
+////    void initAds() {
+////        MobileAds.initialize(context, initializationStatus -> {
+//////            Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
+//////            for (String adapterClass : statusMap.keySet()) {
+//////                AdapterStatus status = statusMap.get(adapterClass);
+//////                Log.d("MyApp", String.format(
+//////                        "Adapter name: %s, Description: %s, Latency: %d",
+//////                        adapterClass, status.getDescription(), status.getLatency()));
+//////            }
+////
+////            // For testing purposes only
+//////            new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("BD62403485F5FEBFDCB000D103922A0C"));
+////
+////            // Start loading ads here...
+////            AdRequest adRequest = new AdRequest.Builder().build();
+////            adView.loadAd(adRequest);
+////        });
+////    }
+//
+////    void pauseAds() {
+////        adView.pause();
+////    }
+////
+////    void resumeAds() {
+////        adView.resume();
+////    }
+//
+//    void destroyAds() {
+//        adView.destroy();
 //    }
-
-    void destroyAds() {
-        adView.destroy();
-    }
-}
+//}
