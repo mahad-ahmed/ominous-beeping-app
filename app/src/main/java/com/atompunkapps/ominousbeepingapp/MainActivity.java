9886359ea0822846a13fa573698fd50e1adad4ec
mahad-ahmed <mahad628@gmail.com>
2024-02-02 08:16:35 +0500

    Removed unused method

--- app/src/main/java/com/atompunkapps/ominousbeepingapp/MainActivity.java
+++ app/src/main/java/com/atompunkapps/ominousbeepingapp/MainActivity.java
@@ -333,8 +333,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         adView.destroy();
         super.onDestroy();
     }
-
-    public String getAdSize() {
-        return "";
-    }
 }
